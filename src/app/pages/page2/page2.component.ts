f78f54f0a6c05d9dda7a47c5acb5acd49dad157f
hazeel <fatimaezzahrakae@gmail.com>
2025-07-19 16:39:40 +0100

    frontend version2

--- src/app/pages/page2/page2.component.ts
+++ src/app/pages/page2/page2.component.ts
@@ -1,32 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page2',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './page2.component.html',
   styleUrls: ['./page2.component.css']
 })
-
 export class Page2Component implements OnInit {
-  progress: number = 0;
-  completed: boolean = false;
-constructor(private router: Router) {}
-  ngOnInit() {
+  completed = false;
+  stageText = 'Preparing the video...';
+  stageIndex = 0;
+  stages = [
+    'Preparing the video...',
+    'Analyzing audio and video...',
+    'Converting to selected format...',
+    'Finalizing...',
+    'Conversion completed successfully!'
+  ];
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.runStages();
+  }
+
+  runStages() {
     const interval = setInterval(() => {
-      this.progress += 5;
-      if (this.progress >= 100) {
-        this.progress = 100;
-        this.completed = true;
+      if (this.stageIndex < this.stages.length - 1) {
+        this.stageIndex++;
+        this.stageText = this.stages[this.stageIndex];
+      } else {
         clearInterval(interval);
-        setTimeout(() => this.router.navigate(['/download']), 1000);
+        this.completed = true;
+        setTimeout(() => this.router.navigate(['/download']), 1000); // auto redirect to Page 3
       }
-    }, 300); 
+    }, 1200); // change stage every 1.2s
   }
 }
-
-
-
-
